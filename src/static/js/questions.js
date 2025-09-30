a0d61537b4702ec90e7258b52dbd3bd9026d1a68
ReTrOBANAN <andreybobrovnsk@yandex.ru>
2025-09-30 20:27:06 +0700

    Добавил форматирование даты

--- src/static/js/questions.js
+++ src/static/js/questions.js
@@ -66,7 +66,7 @@ function toHTML(question) {
             <div class="item-header-name">${question.name} (${question.username})</div>
             <div class="item-header-subject">${question.subject}</div>
             <div class="item-header-grade">${question.grade} класс</div>
-            <div class="item-header-time">${question.created_at}</div>
+            <div class="item-header-time">${timeAgo(question.created_at)}</div>
         </div>
         <div class="questions-item-body">
             ${question.text}
@@ -75,6 +75,32 @@ function toHTML(question) {
             <a class="btn" href="question/${question.id}">Ответить</a>
         </div>
     </li>`
+}
+
+function timeAgo(dateString) {
+    dateString += "Z";
+    const now = new Date();
+    const date = new Date(dateString);
+    const diff = (now - date) / 1000;
+
+    if (diff < 60) {
+        return `${Math.round(diff)} сек. назад`
+    }
+    else if (diff < 3600) {
+        return `${Math.round(diff / 60)} мин. назад`
+    }
+    else if (diff < 86400) {
+        return `${Math.round(diff / 3600)} ч. назад`
+    }
+    else if (diff < 2628000) {
+        return `${Math.round(diff / 86400)} дн. назад`
+    }
+    else if (diff < 31540000){
+        return `${Math.round(diff / 2628000)} мес. назад`
+    }
+    else {
+        return `${date.toLocaleDateString("ru-RU")}`
+    }
 }
 
 const createBtn = document.getElementById('create')
