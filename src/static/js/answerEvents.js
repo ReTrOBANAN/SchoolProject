97ce2fddde82a7e50c363829ac54913fdb1d680b
ReTrOBANAN <andreybobrovnsk@yandex.ru>
2025-10-12 19:07:57 +0700

    Добавил фронтенд изменение/удаление ответов и репорты

---
+++ src/static/js/answerEvents.js
@@ -0,0 +1,148 @@
+const createBtn = document.getElementById('create')
+const overlayContainer = document.getElementById('overlayCreate')
+const closeBtn = document.getElementById('close')
+if (createBtn) {
+    createBtn.addEventListener('click', () => {
+        overlayContainer.classList.add('active')
+    })
+}
+
+
+closeBtn.addEventListener('click', () => {
+    overlayContainer.classList.remove('active')
+})
+
+
+const editBtn = document.getElementById('editBtn')
+const editContainer = document.getElementById('editContainer')
+if (editBtn && editContainer) {
+    editBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        const activeContainers = document.querySelectorAll('.question-edit-container.active');
+        activeContainers.forEach(active => {
+            if (active !== editContainer) active.classList.remove('active');
+        });
+        editContainer.classList.toggle('active');
+    })
+}
+
+answersList.addEventListener('click', (e) => {
+    const editBtn = e.target.closest('#editBtn')
+    const reportAnswerBtn = e.target.closest('#reportAnswerBtn')
+    const deleteAnswerBtn = e.target.closest('#deleteAnswerBtn')
+    const changeAnswerBtn = e.target.closest('#changeAnswerBtn')
+
+    
+    // Обработка кнопки редактирования
+    if (editBtn) {
+        const container = editBtn.closest('.questions-content-item')
+                            .querySelector('.question-edit-container');
+        
+        // Закрываем все активные контейнеры кроме текущего
+        document.querySelectorAll('.question-edit-container.active')
+                .forEach(active => {
+                    if (active !== container) {
+                        active.classList.remove('active');
+                    }
+                });
+        
+        container.classList.toggle('active');
+        e.stopPropagation();
+        return;
+    }
+    
+    // Обработка кнопки жалобы
+    if (reportAnswerBtn) {
+        const overlayReportAnswerContainer = document.getElementById('overlayReportAnswerContainer');
+        const closeReportAnswerContainerBtn = document.getElementById('closeReportAnswerContainerBtn');
+        console.log(overlayChangeQuestionContainer, closeReportAnswerContainerBtn)
+        
+        if (closeReportAnswerContainerBtn && closeReportAnswerContainerBtn) {
+            overlayReportAnswerContainer.classList.add('active');
+            
+            // Убираем дублирование обработчиков
+            closeReportAnswerContainerBtn.onclick = () => {
+                overlayReportAnswerContainer.classList.remove('active');
+            };
+        }
+    }
+    
+    if (deleteAnswerBtn) {
+        const overlaySureAnswerDelete = document.getElementById('overlaySureAnswerDelete')
+        const sureCancelAnswerBtn = document.getElementById('sureCancelAnswerBtn')
+        const sureCloseAnswerBtn = document.getElementById('sureCloseAnswerBtn')
+
+        if (overlaySureAnswerDelete && sureCancelAnswerBtn && sureCloseAnswerBtn) {
+            overlaySureAnswerDelete.classList.add('active')
+            sureCancelAnswerBtn.addEventListener('click', () => {
+                overlaySureAnswerDelete.classList.remove('active')
+            })
+            sureCloseAnswerBtn.addEventListener('click', () => {
+                overlaySureAnswerDelete.classList.remove('active')
+            })
+        }
+    }
+    if (changeAnswerBtn) {
+        const overlayChangeAnswerContainer = document.getElementById('overlayChangeAnswerContainer')
+        const closeChangeAnswerContainerBtn = document.getElementById('closeChangeAnswerContainerBtn')
+        if (overlayChangeAnswerContainer && closeChangeAnswerContainerBtn) {
+            overlayChangeAnswerContainer.classList.add('active')
+            closeChangeAnswerContainerBtn.addEventListener('click', () => {
+                overlayChangeAnswerContainer.classList.remove('active')
+            })
+        }
+    }
+})
+document.addEventListener('click', (e) => {
+    const activeContainers = document.querySelectorAll('.question-edit-container.active');
+
+    activeContainers.forEach(container => {
+        container.classList.remove('active');
+    });
+});
+
+
+
+const deleteQuestionBtn = document.getElementById('deleteQuestionBtn')
+const overlaySureQuestionDelete = document.getElementById('overlaySureQuestionDelete')
+const sureCancelQuestionBtn = document.getElementById('sureCancelQuestionBtn')
+const sureCloseQuestionBtn = document.getElementById('sureCloseQuestionBtn')
+
+if (deleteQuestionBtn && overlaySureQuestionDelete && sureCancelQuestionBtn && sureCloseQuestionBtn) {
+    deleteQuestionBtn.addEventListener('click', () => {
+        overlaySureQuestionDelete.classList.add('active')
+    })
+    sureCancelQuestionBtn.addEventListener('click', () => {
+        overlaySureQuestionDelete.classList.remove('active')
+    })
+    sureCloseQuestionBtn.addEventListener('click', () => {
+        overlaySureQuestionDelete.classList.remove('active')
+    })
+}
+
+
+const changeQuestionBtn = document.getElementById('changeQuestionBtn')
+const overlayChangeQuestionContainer = document.getElementById('overlayChangeQuestionContainer')
+const closeChangeQuestionContainerBtn = document.getElementById('closeChangeQuestionContainerBtn')
+if (changeQuestionBtn && overlayChangeQuestionContainer && closeChangeQuestionContainerBtn) {
+    changeQuestionBtn.addEventListener('click', () => {
+        overlayChangeQuestionContainer.classList.add('active')
+    })
+    closeChangeQuestionContainerBtn.addEventListener('click', () => {
+        overlayChangeQuestionContainer.classList.remove('active')
+    })
+}
+
+
+const reportQuestionBtn = document.getElementById('reportQuestionBtn')
+const overlayReportQuestionContainer = document.getElementById('overlayReportQuestionContainer')
+const closeReportQuestionContainerBtn = document.getElementById('closeReportQuestionContainerBtn')
+if (reportQuestionBtn && overlayReportQuestionContainer) {
+    reportQuestionBtn.addEventListener('click', () => {
+        overlayReportQuestionContainer.classList.add('active')
+    })
+    closeReportQuestionContainerBtn.addEventListener('click', () => {
+        overlayReportQuestionContainer.classList.remove('active')
+    })
+}
---
+++ src/static/js/answerRender.js
@@ -0,0 +1,112 @@
+const questionTime = document.getElementById('questionTime')
+const time = questionTime.dataset.questionTime
+
+questionTime.innerHTML = timeAgo(time)
+
+function timeAgo(dateString) {
+    dateString += "Z";
+    const now = new Date();
+    const date = new Date(dateString);
+    const diff = (now - date) / 1000;
+
+    if (diff < 60) {
+        return `${Math.round(diff)} сек. назад`
+    }
+    else if (diff < 3600) {
+        return `${Math.round(diff / 60)} мин. назад`
+    }
+    else if (diff < 86400) {
+        return `${Math.round(diff / 3600)} ч. назад`
+    }
+    else if (diff < 2628000) {
+        return `${Math.round(diff / 86400)} дн. назад`
+    }
+    else if (diff < 31540000){
+        return `${Math.round(diff / 2628000)} мес. назад`
+    }
+    else {
+        return `${date.toLocaleDateString("ru-RU")}`
+    }
+}
+
+
+const answersList = document.getElementById('answerList')
+async function start() {
+    try {
+        answersList.innerHTML = `<p style="text-align: center; margin-top: 20px">Загрузка ответов...</p>`
+        const response = await fetch('/api/answers')
+        answers = await response.json()
+        render(answers)
+    }
+    catch (err) {
+        answersList.innerHTML = `<p style="text-align: center; margin-top: 20px">Ошибка при загрузке ответов</p>`
+    }
+}
+
+
+
+function render(answers = []) {
+    const questionid = answersList.dataset.questionid
+    answers = answers.filter((answer) => answer.question_id === Number(questionid))
+    if (answers.length === 0) {
+        answersList.innerHTML = `<p style="text-align: center; margin-top: 20px">Нет ответов. Будьте первыми!</p>`
+    }
+    else {
+        const html = answers.map(toHTML).join('')
+        answersList.innerHTML = html
+    }
+}
+
+function toHTML(answer) {
+    const username = answersList.dataset.username
+    if (username === answer.username) {
+        return `<li class="questions-content-item">
+            <button class="edit-btn" id="editBtn">
+                <img src="/static/imgs/more.svg" alt="">
+            </button>
+            <div class="question-edit-container" id="editContainer">
+                <button type="button" class="delete-btn" id="deleteAnswerBtn">
+                    <img src="/static/imgs/delete.svg" alt="">
+                    Удалить
+                </button>
+                <button type="button" class="delete-btn" id="changeAnswerBtn">
+                    <img src="/static/imgs/edit.svg" alt="">
+                    Изменить
+                </button>
+            </div>
+
+            <div class="questions-item-header">
+                <div>${answer.name}</div>
+                <div>${timeAgo(answer.created_at)}</div>
+            </div>
+            <div class="question-text">
+                ${answer.text}
+            </div>
+        </li>
+        `
+    }
+    else {
+        return `<li class="questions-content-item">
+            <button class="edit-btn" id="editBtn">
+                <img src="/static/imgs/more.svg" alt="">
+            </button>
+            <div class="question-edit-container" id="editContainer">
+                <button type="button" class="delete-btn" id="reportAnswerBtn">
+                    <img src="/static/imgs/flag.svg" alt="">
+                    Пожаловаться
+                </button>
+            </div>
+            <div class="questions-item-header">
+                <div>${answer.name}</div>
+                <div>${timeAgo(answer.created_at)}</div>
+            </div>
+            <div class="question-text">
+                ${answer.text}
+            </div>
+        </li>`
+    }
+
+    
+}
+
+start()
